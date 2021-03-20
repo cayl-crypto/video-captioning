72e9ee6b99e8c8983921309d3d775f599caa918e
cayl-crypto <caylio64@gmail.com>
2021-03-20 16:18:54 +0300

    initial commit with requirements

---
+++ utils.py
@@ -0,0 +1,23 @@
+import os
+import sys
+from tqdm import tqdm
+import requests
+from zipfile import ZipFile
+import json
+from PIL import Image
+import matplotlib.pyplot as plt
+
+
+def load_image(path):
+    # reads the image with given path
+
+    return Image.open(path)
+
+
+def show_image(img):
+    # shows the given image
+    img.show()
+
+
+def gray_to_rgb(img):
+    return img.convert('RGB')
