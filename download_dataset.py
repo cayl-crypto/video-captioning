29e02af21cb7f3e8ba3d27ad0fac9ede3f422219
benguftlr <71380207+benguftlr@users.noreply.github.com>
2021-03-23 15:28:04 +0300

    Add files via upload

---
+++ download_dataset.py
@@ -0,0 +1,39 @@
+import os
+import sys
+from tqdm import tqdm
+import requests
+from zipfile import ZipFile
+import json
+from PIL import Image
+import matplotlib.pyplot as plt
+import tarfile
+
+def download_dataset(thetarfile,target_path):
+    print("Downloading...")
+
+    ftpstream = requests.get(thetarfile,stream=True)
+    total_size_in_bytes = int(ftpstream.headers.get('content-length', 0))
+    block_size = 1024  # 1 Kibibyte
+    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+    with open(target_path, 'wb') as f:
+        for data in ftpstream.iter_content(block_size):
+            progress_bar.update(len(data))
+            f.write(data)
+    progress_bar.close()
+    if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+        print("ERROR, something went wrong")
+
+    print("Extracting file...")
+    thetar = tarfile.open(target_path)
+    thetar.extractall()
+    thetar.close()
+    print("Done.")
+
+def main():
+    thetarfile = "https://www.cs.utexas.edu/users/ml/clamp/videoDescription/YouTubeClips.tar"
+    target_path= "C:\\Users\\pc\\PycharmProjects\\video-captioning\\YouTubeClips.tar"
+    download_dataset(thetarfile,target_path)
+
+
+if __name__ == "__main__":
+    main()
---
+++ video_to_frame.py
@@ -0,0 +1,61 @@
+import cv2
+import numpy as np
+import os
+
+def video_to_frame(file_paths):
+
+    path_to_save = 'C:\\Users\\pc\\PycharmProjects\\video-captioning\\Frames'
+    # get file path for desired video and where to save frames locally
+    for i in range(len(file_paths)):
+        cap = cv2.VideoCapture('%s' %(file_paths[i]))
+
+
+        current_frame = 1
+
+        while (True):
+
+            # capture each frame
+            ret, frame = cap.read()
+
+            # Save frame as a jpg file
+            name = 'vid%s_frame' % (i+1) + str(current_frame) + '.jpg'
+            print ('Creating: ' + name)
+            cv2.imwrite(os.path.join(path_to_save, name), frame)
+
+            # keep track of how many images you end up with
+            current_frame += 1
+
+            # stop loop when video ends
+            if not ret:
+                break
+
+        # release capture
+        cap.release()
+    print('Done.')
+
+import os
+
+def get_filepaths(directory):
+
+    file_paths = []  # List which will store all of the full filepaths.
+
+    # Walk the tree.
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            # Join the two strings in order to form the full filepath.
+            filepath = os.path.join(root, filename)
+            file_paths.append(filepath)  # Add it to the list.
+    #print(file_paths)
+    video_to_frame(file_paths)
+    return file_paths  # Self-explanatory.
+
+
+
+
+def main():
+    # Run the above function and store its results in a variable.
+    full_file_paths = get_filepaths("C:\\Users\\pc\\PycharmProjects\\video-captioning\\YouTubeClips")
+
+
+if __name__ == "__main__":
+    main()
