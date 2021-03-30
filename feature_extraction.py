f28226f6d98f64bbd050046d8931064e784ad4fa
benguftlr <71380207+benguftlr@users.noreply.github.com>
2021-03-30 20:51:59 +0300

    Add files via upload

---
+++ feature_extraction.py
@@ -0,0 +1,59 @@
+from __future__ import print_function, division
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+import csv
+import matrix as mx
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+
+DATA_DIR = 'C:\\Users\\pc\\PycharmProjects\\video-captioning\\Frames\\train'
+LOG_DIR = 'logs'
+BATCH_SIZE = 6
+WORKERS = 6
+IMAGE_NET_MEAN = [0.485, 0.456, 0.406]
+IMAGE_NET_STD = [0.229, 0.224, 0.225]
+FREEZE = True
+
+# check for cuda and set gpu or cpu
+device = torch.device("cuda:0" if torch.cuda.is_available() else 'cpu')
+
+transforms = transforms.Compose([
+        transforms.Resize(299),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(IMAGE_NET_MEAN, IMAGE_NET_STD)
+    ])
+
+train_set = datasets.ImageFolder(DATA_DIR,transforms)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=4,
+                                       shuffle=True, num_workers=4)
+classes = train_set.classes
+
+# inception v3
+model = torchvision.models.inception_v3(pretrained=True)
+if FREEZE:
+    for param in model.parameters():
+        param.requires_grad = False
+
+num_ftrs = model.fc.in_features
+model.fc = nn.Linear(num_ftrs, 2)
+model = model.to(device)
+torch.save(model,'features.pt')
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
---
+++ feature_extraction_2.py
@@ -0,0 +1,75 @@
+import shutil
+import subprocess
+import glob
+from tqdm import tqdm
+import numpy as np
+import os
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.jit.annotations import Optional
+from torch import Tensor
+
+import pretrainedmodels
+from pretrainedmodels import utils
+from collections import namedtuple
+
+import Inception
+from Inception import *
+import warnings
+
+
+
+
+def extract_features(frame_steps,model_name,video_path,output_dir,data_dir, model, load_image_fn):
+
+    model.eval()
+
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
+    print("save video feats to %s" % (output_dir))
+
+
+    image_list = sorted(glob.glob(os.path.join(data_dir)))
+    for image in tqdm(image_list):
+        video_id = image.split("/")[-1].split(".")[0]
+    samples = np.round(np.linspace(
+        0, len(image_list) - 1, frame_steps))
+    image_list = [image_list[int(sample)] for sample in samples]
+    images = torch.zeros((len(image_list), 3, 299, 299))
+    for iImg in range(len(image_list)):
+        img = load_image_fn(image_list[iImg])
+        images[iImg] = img
+    with torch.no_grad():
+        fc_feats = model(images.cuda()).squeeze()
+    img_feats = fc_feats.cpu().numpy()
+    # Save the inception features
+    outfile = os.path.join(output_dir, video_id + '.pt')
+    np.save(outfile, img_feats)
+    # cleanup
+    shutil.rmtree(data_dir)
+
+
+if __name__ == '__main__':
+
+
+    data_dir = 'C:\\Users\\pc\\PycharmProjects\\video-captioning\\Frames\\train\\1'
+    output_dir = 'C:\\Users\\pc\\PycharmProjects\\video-captioning\\results'
+
+    model_name = 'inception_v3'
+    frame_steps = 40
+
+    # check for cuda and set gpu or cpu
+    device = torch.device("cuda:0" if torch.cuda.is_available() else 'cpu')
+
+    model = pretrainedmodels.inceptionv3(pretrained='imagenet')
+    #model = Inception.inception_v3(pretrained=False, progress=True)
+    load_image_fn = utils.LoadTransformImage(model)
+
+    model.last_linear = utils.Identity()
+    model = nn.DataParallel(model)
+
+    model = model.cuda()
+
+    extract_features(frame_steps,model_name,data_dir,output_dir, model, load_image_fn)
